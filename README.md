746a8ea8138d8dd975ab27dd7a4582adebbee465
Dan Root <droot@flowroute.com>
2018-01-27 17:46:04 -0800

    Once upon a time...

---
+++ README.md
@@ -0,0 +1,55 @@
+# Twirp Python Server Generator
+
+Generate a Python WSGI application that can answer Twirp requests.
+
+The design of this generator was taken from [Chris Gaffney's ruby client
+generator](https://github.com/gaffneyc/protoc-gen-twirp_ruby).
+
+## Install
+
+```bash
+go get -u github.com/daroot/protoc-gen-twirp_python_srv
+```
+
+## Requirements
+
+Code from the generator requires the following libraries:
+
+- [Werkzeug](http://werkzeug.pocoo.org/) - used for WSGI request parsing.
+- [enum34](https://pypi.python.org/pypi/enum34) - for python3.3 or earlier, including python 2.x
+
+Additionally in order to use the resulting applicaiton,  you'll need a
+standalone WSGI capable server (such as gunicorn or uwsgi) or a library capable
+of acting as a server (like bjoern, gevent, or werkzeug's internal debugging
+server).
+
+## Usage
+
+```bash
+protoc --proto_path=$GOPATH/src:. --twirp_python_srv_out=. --python_out=. path/to/service.proto
+```
+
+Create service, using [bjoern](https://github.com/jonashaag/bjoern) as a WSGI
+server:
+
+```python
+import random
+
+import bjoern
+import haberdasher_pb2 as pb
+from haberdasher_pb2_twirp import (Errors, HaberdasherImpl, HaberdasherServer,
+								   TwirpException)
+
+class MadHaberdasher(HaberdasherImpl):
+	def MakeHat(self, size):
+		if size.Inches <= 0:
+			raise TwirpException(Errors.InvalidArgument,
+			                     "I can't make a hat that small")
+		return pb.Hat(Size=size.Inches,
+					  Color=random.choice("white", "black", "brown", "red"),
+					  Name=random.choice("bowler", "top hat", "derby"))
+
+if __name__ == "__main__":
+	app = HaberdasherServer(MadHaberdasher())
+	bjoern.run(app, "0.0.0.0", 8080)
+```
---
+++ generator.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"path"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+	"github.com/pkg/errors"
+)
+
+type generator struct{}
+
+func (g *generator) Generate(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, error) {
+	resp := &plugin.CodeGeneratorResponse{}
+
+	for _, name := range req.FileToGenerate {
+		file, err := getFileDescriptor(req, name)
+		if err != nil {
+			return nil, err
+		}
+
+		genFile, err := g.generateFile(file)
+		if err != nil {
+			return nil, errors.Wrapf(err, "generating %q", name)
+		}
+
+		// Add the generated file to the response
+		resp.File = append(resp.File, genFile)
+	}
+
+	return resp, nil
+}
+
+func (g *generator) generateFile(file *descriptor.FileDescriptorProto) (*plugin.CodeGeneratorResponse_File, error) {
+	var err error
+
+	buffer := &bytes.Buffer{}
+
+	tmpl := template.New("python_file")
+	tmpl, err = tmpl.Parse(pythonSrvTemplate)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing template")
+	}
+
+	presenter := &pythonSrvTemplatePresenter{
+		proto: file,
+
+		Version:        "1.0.0",
+		SourceFilename: file.GetName(),
+		Package:        file.GetPackage(),
+	}
+
+	err = tmpl.Execute(buffer, presenter)
+	if err != nil {
+		return nil, errors.Wrapf(err, "rendering template for %q", file.GetName())
+	}
+
+	resp := &plugin.CodeGeneratorResponse_File{}
+	resp.Name = proto.String(getOutputFilename(file))
+	resp.Content = proto.String(buffer.String())
+
+	return resp, nil
+}
+
+// getFileDescriptor finds the FileDescriptorProto for the given filename.
+// Returns a error if the descriptor could not be found.
+func getFileDescriptor(req *plugin.CodeGeneratorRequest, name string) (*descriptor.FileDescriptorProto, error) {
+	for _, descriptor := range req.ProtoFile {
+		if descriptor.GetName() == name {
+			return descriptor, nil
+		}
+	}
+
+	return nil, errors.Errorf("could not find descriptor for %q", name)
+}
+
+// getOutputFilename determines what the filename should be for the generated
+// code.
+func getOutputFilename(file *descriptor.FileDescriptorProto) string {
+	name := file.GetName()
+	name = strings.TrimSuffix(name, path.Ext(name))
+
+	return name + "_twirp_srv.py"
+}
